55ecf3dac495ebd85742cb173422567d091fe106
angieappiah <alegnalawn2@yahoo.com>
2020-11-08 16:04:31 -0800

    commit tree 99bd0ffad0c51ef5c31f3aa32b2580bc9a9f234e

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  
+]
+superbowlWin.find (function(s) {return s === "win"})
+
